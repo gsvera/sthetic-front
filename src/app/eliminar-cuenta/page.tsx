a93ad70a0860770f4c8a8d1186febb5e22da32d3
gsvera <55720644+gsvera@users.noreply.github.com>
2025-09-19 00:02:26 -0500

    feat / seccion eliminar cuenta

---
+++ src/app/eliminar-cuenta/page.tsx
@@ -0,0 +1,20 @@
+export default function RequestDeleteAccount() {
+  return (
+    <div className="container-body-delete">
+      <h1 className="title-delete-account">Eliminacion de cuenta</h1>
+      <div className="content-text-delete-account">
+        <p className="text-paragraph line-height-m">
+          Para eliminar tu cuenta te invitamos que lo hagas directo en la
+          aplicacion en el apartado de configuracion y en la opcion de eliminar
+          cuenta para evitar malos entendidos, si no te es posible por alguna
+          razon te invitamos que nos escribas desde el email que registraste en
+          la applicacion a nuestro email{" "}
+          <span className="font-bold">meredith.abasotech@gmail.com</span> y con
+          gusto te ayudaremos. Te pedimos que agregues tu motivo por el cual
+          quieres eliminar tu cuenta, esto nos ayudara a mejorar nuestra
+          aplicacion.
+        </p>
+      </div>
+    </div>
+  );
+}
