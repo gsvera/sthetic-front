ed48346b9621d99ba6fcf5278e58accceafa72a3
gsvera <55720644+gsvera@users.noreply.github.com>
2025-11-19 14:32:50 -0500

    fix / suspence para vista registro de suscripcion

--- src/app/registro-de-subscripcion/page.tsx
+++ src/app/registro-de-subscripcion/page.tsx
@@ -1,10 +1,20 @@
+import LoadingView from "@/components/WebComponents/LoadingView";
 import SubscriptionModule from "@/components/WebComponents/SubscriptionModule";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { Suspense } from "react";
 
 export default function Subcription() {
   return (
-    <AntdRegistry>
-      <SubscriptionModule />
-    </AntdRegistry>
+    <Suspense
+      fallback={
+        <div className="conent-loading-height">
+          <LoadingView />
+        </div>
+      }
+    >
+      <AntdRegistry>
+        <SubscriptionModule />
+      </AntdRegistry>
+    </Suspense>
   );
 }
